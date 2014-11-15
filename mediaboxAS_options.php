ea7df471728c808ba9ed2859672a770f6208b0f7
Debondt Didier <p@henix.be>
2014-11-16 00:18:29 +0100

    Ménage

--- mediaboxAS_options.php
+++ mediaboxAS_options.php
@@ -10,15 +10,3 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-
-
-/*
- * Un fichier d'options permet de définir des éléments
- * systématiquement chargés à chaque hit sur SPIP.
- *
- * Il vaut donc mieux limiter au maximum son usage
- * tout comme son volume !
- * 
- */
-
-?>
