22a74ec3205d9d24f0fddc51571f080980d6baa9
Debondt Didier <p@henix.be>
2014-11-16 00:19:13 +0100

    Ajouter les options de la laighbox aria

--- javascript/spip_lightbox_aria.js
+++ javascript/spip_lightbox_aria.js
@@ -1,3 +1,16 @@
 $(function () {
-    $(".mediabox, a[type='image/jpeg'], a[type='image/png'], a[type='image/gif']").ariaLightbox();
+    $(".mediabox, a[type='image/jpeg'], a[type='image/png'], a[type='image/gif']").ariaLightbox({
+        altText: function() {
+					  return $(this).attr("title");
+				},
+				descText: function() {
+					  return $(this).attr("title");
+				},
+				titleText: function() {
+					return $(this).attr("title");
+				},
+				useDimmer: true,
+				pos: "auto",
+				em: false
+    });
 });
